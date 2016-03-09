5ab4db29e41311370fba67a5ac69b917ccbf6948
Florian Huet <florian.huet@epitech.eu>
2016-03-09 21:29:01 +0100

    first push

---
+++ Assets/CodingClub/_Scipts/ObuBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ObuBehaviour : NetworkBehaviour {
+	
+	[SyncVar]
+	public Quaternion direction;
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+		this.transform.rotation = direction;
+		transform.Translate(Vector3.forward * 25 * Time.deltaTime, Space.Self);
+		
+	}
+}
